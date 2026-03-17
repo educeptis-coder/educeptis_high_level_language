3de46eaee2675a66a39a205f5ba3dcae5decb8a4
educeptis-coder <educeptis@gmail.com>
2026-03-17 03:05:58 +0100

    hey

--- module2/8-islower.py
+++ module2/8-islower.py
@@ -1,3 +1,5 @@
 def islower(c):
     """Return True if c is a lowercase letter, False otherwise."""
-    return ord('a') <= ord(c) <= ord('z')
+    if ord('a') <= ord(c) <= ord('z'):
+        return True
+    return False
